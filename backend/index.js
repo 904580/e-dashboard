0db4fdd154256d355f90509588e2029cc86e9189
Anoop Saini <Anoopsaini421@gmail.com>
2022-02-25 15:39:37 +0530

    add by me today

--- backend/index.js
+++ backend/index.js
@@ -55,6 +55,51 @@ app.post("/add-product", async (req,res)=>{
 
 })
 
+app.get("/products", async(req,res)=>{
+     let products = await Product.find();
+     if(products.length>0){
+         res.send(products)
+     }else{
+         res.send({result:"No Products found"})
+     }
+
+})
+
+app.delete("/product/:id",async(req,res)=>{
+    const result = await Product.deleteOne({_id:req.params.id})
+    res.send(result);
+});
+
+app.get("/product/:id",async(req,res)=>{
+    let result = await Product.findOne({_id:req.params.id});
+    if(result){
+        res.send(result)
+    }else{
+        res.send({result:"No Record Found.."})
+    }
+    
+})
+
+app.put("/product/:id",async(req,res)=>{
+    let result = await Product.updateOne(
+        {_id:req.params.id},
+        {
+            $set:req.body
+        }
+    )
+    res.send(result)
+});
+
+app.get("/search/:key",async (req,res)=>{
+    let result = await Product.find({
+        "$or":[
+            {name:{$regex:req.params.key}},
+            {company:{$regex:req.params.key}},
+            {category:{$regex:req.params.key}},
+        ]
+    });
+    res.send(result)
+})
 
 app.listen(7000, () => {
     console.log("listening the port 7000");
